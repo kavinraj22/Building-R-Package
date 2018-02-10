1d72edbb702cdcc92f02b193b67d41b24764a94e
kavinraj22 <kavin.kng@gmail.com>
2018-02-09 20:30:15 -0600

    Add files via upload

---
+++ MyPackage/R/Square.R
@@ -0,0 +1,21 @@
+#' Square of a number
+#'
+#' Takes in any numeric value and squares it
+#' @param A numeric value to be squared
+#' @return The square of the input
+#' @export
+
+square <- function(x) {
+  return(x^2)
+}
+
+#' Cube of a number
+#'
+#' Takes in any numeric value and cube it
+#' @param A numeric value to be squared
+#' @return The cube of the input
+#' @export
+#'
+cube <- function(y) {
+  return(y^3)
+}
